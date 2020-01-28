e0d7066c1e3c3aa53c61afe62ae8446967dad253
saahilmehra <saahilmehra1998@gmail.com>
2020-01-28 21:34:40 +0530

    User can now add blogs.

--- blogshare/blog/urls.py
+++ blogshare/blog/urls.py
@@ -7,4 +7,5 @@ app_name='blog'
 urlpatterns=[
     path('', login_required(views.PostListView.as_view()), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('add/', views.addPost, name='add_post')
 ]
--- blogshare/blog/views.py
+++ blogshare/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
+from .forms import AddPostForm
 
 # Create your views here.
 class PostListView(ListView):
@@ -23,3 +24,21 @@ def post_detail(request, year, month, day, slug):
         return render(request, 'blog/post/detail.html', {'post':post, 'has_access':has_access})
 
     return render(request, 'blog/post/detail.html', {'has_access':has_access})
+
+@login_required
+def addPost(request):
+    if request.method=='POST':
+        form=AddPostForm(request.POST)
+        if form.is_valid():
+            #create a new post object but dont save yet
+            new_post=form.save(commit=False)
+            #Add author
+            new_post.author=request.user
+            #now save
+            new_post.save()
+
+            return redirect('../')
+            #return redirect(post_detail, year=new_post.publish.year, month=new_post.publish.month, day=new_post.publish.day, slug=new_post.slug)
+    else:
+        form=AddPostForm()
+    return render(request, 'blog/post/add_post.html', {'form':form})
