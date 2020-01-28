ceefce2679fc40e8b15004f92880d55836c0d0a2
saahilmehra <saahilmehra1998@gmail.com>
2020-01-28 09:11:24 +0530

    users can now signup.

--- blogshare/account/forms.py
+++ blogshare/account/forms.py
@@ -1,1 +1,16 @@
 from django import forms
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+class UserRegistrationForm(forms.ModelForm):
+    password=forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2=forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model=get_user_model()
+        fields=('email', 'first_name', 'last_name', 'contact')
+
+    def clean_password2(self):
+        cd=self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match')
--- blogshare/account/urls.py
+++ blogshare/account/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
-    path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
