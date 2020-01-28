d4b6b9ceae695bbd1a00a9e92bf219ff96d9bb92
saahilmehra <saahilmehra1998@gmail.com>
2020-01-28 18:40:09 +0530

    Users can now view their own blog.

--- blogshare/blog/models.py
+++ blogshare/blog/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your models here.
 modeluser=get_user_model()
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES=(
@@ -20,8 +25,14 @@ class Post(models.Model):
     updated=models.DateTimeField(auto_now=True)
     status=models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects=models.Manager()
+    published=PublishedManager()
+
     class Meta:
         ordering=('-publish',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
--- blogshare/blog/views.py
+++ blogshare/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def post_list(request):
+    posts=Post.published.filter(author__email=request.user)
+    return render(request, 'blog_post/list.html', {'posts':posts})
+
+@login_required
+def post_detail(request, year, month, day, slug):
+    has_access=False
+    raw_post=get_object_or_404(Post, slug=slug, status='published', publish__year=year, publish__month=month, publish__day=day)
+
+    if raw_post.author == request.user:
+        has_access=True
+        post=raw_post
+        return render(request, 'blog_post/detail.html', {'post':post, 'has_access':has_access})
+
+    return render(request, 'blog_post/detail.html', {'has_access':has_access})
