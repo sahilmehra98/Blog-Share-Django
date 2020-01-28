03d7d587d04e646b6cbe04fff93132fa9b4dd479
saahilmehra <saahilmehra1998@gmail.com>
2020-01-28 19:38:17 +0530

    Added pagination.

--- blogshare/blog/views.py
+++ blogshare/blog/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 @login_required
 def post_list(request):
-    posts=Post.published.filter(author__email=request.user)
-    return render(request, 'blog_post/list.html', {'posts':posts})
+    object_list=Post.published.filter(author__email=request.user)
+    #3 posts per page
+    paginator=Paginator(object_list, 3)
+    page=request.GET.get('page')
+    try:
+        posts=paginator.page(page)
+    except PageNotAnInteger:
+        #If page is not an integer, deliver the first page
+        posts=paginator.page(1)
+    except EmptyPage:
+        #if page is out of range, deliver last page of results
+        posts=paginator.page(paginator.num_pages)
+
+    return render(request, 'blog_post/list.html', {'posts':posts, 'page':page})
 
 @login_required
 def post_detail(request, year, month, day, slug):
