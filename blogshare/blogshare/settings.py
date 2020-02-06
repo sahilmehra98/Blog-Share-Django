2c2187f5869e444176d2413da56061a264376470
saahilmehra <saahilmehra1998@gmail.com>
2020-02-07 00:44:27 +0530

    Added django rest framework.

--- blogshare/blogshare/settings.py
+++ blogshare/blogshare/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
